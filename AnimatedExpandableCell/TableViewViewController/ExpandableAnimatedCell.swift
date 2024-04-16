574bf1081885e2d79752086e8d7f84a30678c403
Abdallah Mahameed <a.mahameed2000@gmail.com>
2024-04-16 08:47:54 +0300

    animation with spring effect

--- AnimatedExpandableCell/TableViewViewController/ExpandableAnimatedCell.swift
+++ AnimatedExpandableCell/TableViewViewController/ExpandableAnimatedCell.swift
@@ -11,19 +11,35 @@ class ExpandableAnimatedCell: UITableViewCell {
         
     static let cellID = String(describing: ExpandableAnimatedCell.self)
     
-    fileprivate var title: UILabel = {
+    func animateCell() {
+        UIView.animate(withDuration: 0.3, delay: 0.5, usingSpringWithDamping: 0.8, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
+            self.contentView.layoutIfNeeded()
+        })
+    }
+    
+    var cellData: Cell? {
+        didSet {
+            guard let cellData = cellData else { return }
+            title.text = cellData.title
+            subTitle.text = cellData.subTitle
+        }
+    }
+    
+    private let title: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
         label.textAlignment = .center
+        return label
     }()
     
-    fileprivate var subTitle: UILabel = {
+    private let subTitle: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.font = UIFont.systemFont(ofSize: 20, weight: .heavy)
         label.textAlignment = .center
         label.numberOfLines = -1
+        return label
     }()
     
     private let container: UIView = {
@@ -32,6 +48,7 @@ class ExpandableAnimatedCell: UITableViewCell {
         view.clipsToBounds = true
         view.layer.cornerRadius = 8
         view.backgroundColor = .systemBlue
+        return view
     }()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -40,22 +57,25 @@ class ExpandableAnimatedCell: UITableViewCell {
         contentView.addSubview(container)
         
         NSLayoutConstraint.activate([
-            container.topAnchor.constraint(equalTo: topAnchor, constant: 5),
-            container.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 5),
+            container.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 5),
+            container.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 5),
             contentView.trailingAnchor.constraint(equalTo: container.trailingAnchor, constant: 5),
             contentView.bottomAnchor.constraint(equalTo: container.bottomAnchor, constant: 5)
         ])
         
         container.addSubview(title)
         container.addSubview(subTitle)
+        
+        title.topAnchor.constraint(equalTo: container.topAnchor).isActive = true
+        title.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+        title.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        title.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        subTitle.topAnchor.constraint(equalTo: title.bottomAnchor).isActive = true
+        subTitle.leadingAnchor.constraint(equalTo: container.leadingAnchor).isActive = true
+        subTitle.trailingAnchor.constraint(equalTo: container.trailingAnchor).isActive = true
+        subTitle.heightAnchor.constraint(equalToConstant: 140).isActive = true
     }
-    
-    fileprivate func animateCell() {
-        UIView.animate(withDuration: 0.4, delay: 0.2, usingSpringWithDamping: 0.5, initialSpringVelocity: 1) {
-            self.contentView.layoutIfNeeded()
-        }
-    }
-    
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- AnimatedExpandableCell/TableViewViewController/ViewController.swift
+++ AnimatedExpandableCell/TableViewViewController/ViewController.swift
@@ -12,11 +12,11 @@ class ViewController: UITableViewController {
     private var selectedIndex = IndexPath(row: 0, section: 0)
     
     fileprivate var cellData = [
-        Cell(title: "Mustafa", subTitle: "60"),
-        Cell(title: "Laith", subTitle: "33"),
+        Cell(title: "Jack", subTitle: "47"),
+        Cell(title: "Daniel", subTitle: "33"),
         Cell(title: "Abdallah", subTitle: "23")
     ]
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setupTableView()
@@ -25,26 +25,40 @@ class ViewController: UITableViewController {
     private func setupTableView() {
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.register(ExpandableAnimatedCell.self, forCellReuseIdentifier: ExpandableAnimatedCell.cellID)
+        tableView.separatorStyle = .none
     }
 }
 
 extension ViewController {
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return if selectedIndex == indexPath{
-            200
-        } else {
-            60
+        if selectedIndex == indexPath {
+            return 200
         }
-    }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: ExpandableAnimatedCell.cellID)
+        return 60
         
     }
     
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellData.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ExpandableAnimatedCell.cellID) as! ExpandableAnimatedCell
+       
+        cell.cellData = cellData[indexPath.row]
+        cell.selectionStyle = .none
+        cell.animateCell()
+        return cell
+    }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        <#code#>
+        selectedIndex = indexPath
+        
+        tableView.beginUpdates()
+        tableView.reloadRows(at: [indexPath], with: .none)
+        tableView.endUpdates()
+        
     }
     
 }
